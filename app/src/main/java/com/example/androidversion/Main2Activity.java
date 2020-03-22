2b53c4057ef1ed2560b1510972a4527be2e3c27f
song2 <msms9957@gmail.com>
2020-03-22 13:14:21 +0900

    20200322

--- app/src/main/java/com/example/androidversion/Main2Activity.java
+++ app/src/main/java/com/example/androidversion/Main2Activity.java
@@ -1,13 +1,15 @@
 package com.example.androidversion;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -18,38 +20,15 @@ public class Main2Activity extends AppCompatActivity {
     private ListView main2_list;
     private NoticeAdapter adapter;
     private List<Notice> noticedList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        setTitle("과업님! 키퍼의 메인페이지입니다.");
-        Button main2_logoutbtn=findViewById(R.id.main2_logoutbtn);
-        Button main2_mypagebtn=findViewById(R.id.main2_mypagebtn);
-        Button main2_searchbtn=findViewById(R.id.main2_searchbtn);
-
-        main2_logoutbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"로그아웃 중...",Toast.LENGTH_LONG).show();
-                Intent intent5=new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent5);
-                finish();
-            }
-        });
-        main2_mypagebtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent6 =new Intent(getApplicationContext(),MypageActivity.class);
-                startActivity(intent6);
-            }
-        });
-        main2_searchbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent7=new Intent(getApplicationContext(),SearchActivity.class);
-                startActivity(intent7);
-            }
-        });
+        Toolbar toolbar=(Toolbar)findViewById(R.id.app_toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         main2_list = (ListView)findViewById(R.id.main2_list);
         noticedList = new ArrayList<Notice>();
@@ -57,6 +36,8 @@ public class Main2Activity extends AppCompatActivity {
         adapter = new NoticeAdapter(getApplicationContext(), noticedList);
         main2_list.setAdapter(adapter);
     }
+
+
     private void main2_setList() {
         noticedList = new ArrayList<Notice>();
         noticedList.add(new Notice("공지사항", "이준범", "2018-09-09"));
@@ -71,5 +52,43 @@ public class Main2Activity extends AppCompatActivity {
         noticedList.add(new Notice("공지사항", "이상원", "2018-09-18"));
         noticedList.add(new Notice("공지사항", "이재갑", "2018-09-19"));
         noticedList.add(new Notice("공지사항", "김혜진", "2018-09-20"));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_main2, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.action_search:
+                Intent intent7=new Intent(getApplicationContext(),SearchActivity.class);
+                startActivity(intent7);
+                return true;
+
+            case R.id.action_mypage:
+                Intent intent6 =new Intent(getApplicationContext(),MypageActivity.class);
+                startActivity(intent6);
+                return true;
+
+            case R.id.action_setting:
+                Toast.makeText(getApplicationContext(), "설정", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_logout:
+                Toast.makeText(getApplicationContext(),"로그아웃 중...",Toast.LENGTH_LONG).show();
+                Intent intent5=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent5);
+                finish();
+                return true;
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
